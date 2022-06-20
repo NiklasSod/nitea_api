04f7d1df7688ae2c3cd196c216ec927adc7481a7
NiklasSod <nma.prob@gmail.com>
2022-06-20 21:07:44 +0200

    post method

--- index.php
+++ index.php
@@ -23,20 +23,31 @@
             echo json_encode($products);
             break;
 
-        // case "POST":
-        //     $product = json_decode(file_get_contents('php://input'));
-        //     $sql = "INSERT INTO products(product_id, product_name, product_url, product_price, currency_id) 
-        //     VALUES(null, :product_name, :product_url, :product_price, :currency_id)";
-        //     $stmt = $conn->prepare($sql);
-        //     $stmt->bindParam(':product_name', $product->product_name);
-        //     $stmt->bindParam(':product_url', $product->product_url);
-        //     $stmt->bindParam(':product_price', $product->product_price);
-        //     $stmt->bindParam(':currency_id', $product->currency_id);
-        //     if($stmt->execute()){
-        //         $response = ['status' => 1, 'message' => 'Product created!'];
-        //     } else {
-        //         $response = ['status' => 0, 'message' => 'Failed to create product.'];
-        //     }
-        //     echo json_encode($response);
-        //     break;
+        case "POST":
+            $product = json_decode(file_get_contents('php://input'));
+            $sql = "INSERT INTO products(product_id, product_name, product_url, product_price, currency_id)
+                VALUES(null, :product_name, :product_url, :product_price, :currency_id);";
+            
+            $sql2 = "INSERT INTO product_categories(product_id, category_id)
+                VALUES(LAST_INSERT_ID(), :category_id);";
+            $stmt = $conn->prepare($sql);
+            $stmt2 = $conn->prepare($sql2);
+            $stmt->bindParam(':product_name', $product->product_name);
+            $stmt->bindParam(':product_url', $product->product_url);
+            $stmt->bindParam(':product_price', $product->product_price);
+            $stmt->bindParam(':currency_id', $product->currency_id);
+            $stmt2->bindParam(':category_id', $product->category_id);
+            if($stmt->execute()){
+                $response = ['status' => 1, 'message' => 'Product created!'];
+            } else {
+                $response = ['status' => 0, 'message' => 'Failed to create product.'];
+            }
+            if($stmt2->execute()){
+                $response2 = ['status' => 1, 'message' => 'Category created!'];
+            } else {
+                $response2 = ['status' => 0, 'message' => 'Failed to create category.'];
+            }
+            echo json_encode($response);
+            echo json_encode($response2);
+            break;
     }
