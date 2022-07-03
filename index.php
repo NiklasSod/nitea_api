bbce0f1b39caa5c002024974c0d59f838f507d5b
NiklasSod <nma.prob@gmail.com>
2022-07-03 14:05:15 +0200

    add transaction to POST

--- index.php
+++ index.php
@@ -43,27 +43,34 @@
             break;
 
         case "POST":
-            $product = json_decode(file_get_contents('php://input'));
-            $sql = "INSERT INTO products(product_id, product_name, product_url, product_price, currency_id, release_status)
-                VALUES(null, :product_name, :product_url, :product_price, :currency_id, :release_status);";
-            
-            $sql2 = "INSERT INTO product_categories(product_id, category_id)
-                VALUES(LAST_INSERT_ID(), :category_id);";
-            $stmt = $conn->prepare($sql);
-            $stmt2 = $conn->prepare($sql2);
-            $stmt->bindParam(':product_name', $product->product_name);
-            $stmt->bindParam(':product_url', $product->product_url);
-            $stmt->bindParam(':product_price', $product->product_price);
-            $stmt->bindParam(':currency_id', $product->currency_id);
-            $stmt->bindParam(':release_status', $product->released);
-            $stmt->execute();
+            try {
+                $conn->beginTransaction();
+                $product = json_decode(file_get_contents('php://input'));
+                $sql = "INSERT INTO products(product_id, product_name, product_url, product_price, currency_id, release_status)
+                    VALUES(null, :product_name, :product_url, :product_price, :currency_id, :release_status);";
+                
+                $sql2 = "INSERT INTO product_categories(product_id, category_id)
+                    VALUES(LAST_INSERT_ID(), :category_id);";
+                $stmt = $conn->prepare($sql);
+                $stmt2 = $conn->prepare($sql2);
+                $stmt->bindParam(':product_name', $product->product_name);
+                $stmt->bindParam(':product_url', $product->product_url);
+                $stmt->bindParam(':product_price', $product->product_price);
+                $stmt->bindParam(':currency_id', $product->currency_id);
+                $stmt->bindParam(':release_status', $product->released);
+                $stmt->execute();
 
-            $j = count($product->category_id);
-            for($i = 0; $i < $j; $i++){
-                $stmt2->bindParam(':category_id', $product->category_id[$i]);
-                $stmt2->execute();
-            };
-            break;
+                $j = count($product->category_id);
+                for($i = 0; $i < $j; $i++){
+                    $stmt2->bindParam(':category_id', $product->category_id[$i]);
+                    $stmt2->execute();
+                };
+                $conn->commit();
+                break;
+            } catch (PDOException $e) {
+                $conn->rollBack();
+                die($e->getMessage());
+            }
 
         case "PUT":
             $product = json_decode(file_get_contents('php://input'));
